45512df04aacbc31089524bc04ea8d1911adfc5b
onions41 <kblee89@outlook.com>
2023-06-25 12:03:34 -0700

    Added logo and arranged menu items

--- react_frontend/src/menu/Menu.jsx
+++ react_frontend/src/menu/Menu.jsx
@@ -2,7 +2,9 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 // The top-bar menu
 export default function Menu({ setIsAddProdModalOpen }) {
@@ -13,20 +15,49 @@ export default function Menu({ setIsAddProdModalOpen }) {
         height: "60px",
         backgroundColor: "#003366",
         boxShadow: "0px 1px 4px 0px #727272",
-        WebkitBoxShadow: "0px 1px 4px 0px #727272"
+        WebkitBoxShadow: "0px 1px 4px 0px #727272",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center"
       }}
     >
       <Container
         maxWidth="xl"
         // root edited to remove default horizontal padding
         sx={{
-          "&.MuiContainer-root": { padding: "0px" }
+          "&.MuiContainer-root": { padding: "0px" },
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center"
         }}
       >
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={2}
+        >
+          <a href="https://github.com/onions41/Kwan-Lee-ecc-dssb-IS21-code-challenge-req101408">
+            <img
+              src="round_bird_192x192.png"
+              alt="A Logo"
+              width="36"
+              style={{ borderRadius: "50%" }}
+            />
+          </a>
+          <Typography
+            pt="1px"
+            variant="body1"
+            component="h1"
+            color="grey.200"
+          >
+            Kwan-Lee-ecc-dssb-IS21-code-challenge-req101408
+          </Typography>
+        </Stack>
         <Button
           variant="contained"
-          startIcon={<DeleteIcon />}
+          startIcon={<AddIcon />}
           onClick={() => setIsAddProdModalOpen(true)}
+          color="success"
         >
           Add Product
         </Button>
