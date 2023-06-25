ce8357bb419e0c9bc840f8311b63a938ad274238
onions41 <kblee89@outlook.com>
2023-06-25 11:04:29 -0700

    Styled bar

--- react_frontend/src/menu/Menu.jsx
+++ react_frontend/src/menu/Menu.jsx
@@ -1,19 +1,36 @@
 // MUI (UI components)
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 // The top-bar menu
 export default function Menu({ setIsAddProdModalOpen }) {
   return (
-    <Box sx={{ width: "100%", height: "70px", backgroundColor: "plum" }}>
-      <Button
-        variant="contained"
-        startIcon={<DeleteIcon />}
-        onClick={() => setIsAddProdModalOpen(true)}
+    <Box
+      sx={{
+        width: "100%",
+        height: "60px",
+        backgroundColor: "#003366",
+        boxShadow: "0px 1px 4px 0px #727272",
+        WebkitBoxShadow: "0px 1px 4px 0px #727272"
+      }}
+    >
+      <Container
+        maxWidth="xl"
+        // root edited to remove default horizontal padding
+        sx={{
+          "&.MuiContainer-root": { padding: "0px" }
+        }}
       >
-        Add Product
-      </Button>
+        <Button
+          variant="contained"
+          startIcon={<DeleteIcon />}
+          onClick={() => setIsAddProdModalOpen(true)}
+        >
+          Add Product
+        </Button>
+      </Container>
     </Box>
   );
 }
