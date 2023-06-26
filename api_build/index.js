30f9bda2cf1f851fdbe3911a61c2c5444a04588d
onions41 <kblee89@outlook.com>
2023-06-26 14:13:47 -0700

    Built API and frontend

--- api_build/index.js
+++ api_build/index.js
@@ -1,13 +1,20 @@
 "use strict";
 
-// Imports
 const express = require("express");
 const path = require("path");
+const crypto = require("crypto");
+
+// Internal imports
 const mockData = require("./mockData");
+const validateFormInput = require("./validateFormInput");
+
+// Initializes server
 const app = express();
 
-// CORS. Only needed during development
-// undefined in production build
+// Adding middleware
+
+// CORS. Only used during development
+// REACT_CLIENT_ORIGIN is undefined in production build
 if (process.env.REACT_CLIENT_ORIGIN) {
   const cors = require("cors");
   app.use(cors({
@@ -15,47 +22,102 @@ if (process.env.REACT_CLIENT_ORIGIN) {
   }));
 }
 
+// Parses JSON in request body
+app.use(express.json());
+
+// Serving the frontend application
+
 // Serves static files required by the frontend
 app.use(express.static(path.join(__dirname, "..", "react_frontend", "build")));
-// Parses JSON bodies
-app.use(express.json());
 // Serves frontend bundled JavaScript
 app.get("/", function (_req, res) {
   res.sendFile(path.join(__dirname, "..", "react_frontend", "build", "index.html"));
 });
 
-// Read all products
+// Defining API endpoints
+
+// Get all products
 app.get("/api/product", function (_req, res) {
   try {
+    // Healthy response. Sends back mock data in response as JSON
     res.status(200).json(mockData);
   } catch (error) {
-    res.status(500).send(error);
+    // Server error. Sends back error message in response
+    res.status(500).send(error.message);
   }
 });
 
 // Create a new product
 app.post("/api/product", function (req, res) {
   try {
-    // TODO: Validate request body, should fit data shape. throw error if not
-    mockData.push(req.body);
-    res.send(200).json({
-      wasSuccessful: true,
-      data: req.body
-    });
+    // Input validation
+    if (!validateFormInput(req.body)) {
+      throw new Error("Invalid input. Form was not filled out correctly", {
+        cause: "bad input"
+      });
+    }
+
+    // Passed Validation
+
+    // Generate a product ID for the new product
+    req.body.productId = crypto.randomUUID();
+    // Add the new product to the mock database
+    mockData.splice(0, 0, req.body);
+    // Respond with the new product that now has a product ID
+    res.status(200).json(req.body);
   } catch (error) {
-    res.status(400).json({
-      wasSuccessful: false,
-      error
-    });
+    // User fault error
+    if ("cause" in error && error.cause === "bad input") {
+      res.status(422).send(error.message);
+    }
+    // All other errors
+    res.status(500).send(error.message);
+  }
+});
+
+// Edit an existing product
+app.put("/api/product/:productId", function (req, res) {
+  try {
+    // Input validation
+    if (!validateFormInput(req.body)) {
+      throw new Error("Invalid input. Form was not filled out correctly", {
+        cause: "bad input"
+      });
+    }
+    // Looks for product to edit. Throw error if not found
+    const index = mockData.findIndex(row => row.productId === req.params.productId);
+    if (index === -1) {
+      throw new Error("Could not find an existing product with matching Product Number", {
+        cause: "bad input"
+      });
+    }
+
+    // Passed validation
+
+    // Edits the product and responds with the result
+    mockData[index] = {
+      ...mockData[index],
+      ...req.body
+    };
+    res.status(200).json(mockData[index]);
+  } catch (error) {
+    // User fault error
+    if ("cause" in error && error.cause === "bad input") {
+      res.status(422).send(error.message);
+    }
+    // All other errors
+    res.status(500).send(error.message);
   }
 });
 
 // Catch all for unused routes and verbs
 app.use((_req, res) => {
-  res.status(404).send("404 - This endpoint and verb is not used");
+  res.status(404).send("404 - Not Found");
 });
 
-// Start the server
+// Starting the server
+
+// Listen on port 3000
 app.listen(3000, () => {
   console.log("API is running on port 3000");
 });
---
+++ api_build/validateFormInput.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * Validates the request body, which is supposed to be a JSON object
+ * that contains certain keys. This is a bare-bones validation. It can
+ * be extended to make validation much more robust if needed in future.
+ */
+function validateFormInput(reqBody) {
+  if (typeof reqBody !== "object" || reqBody === null) {
+    // Not an object or array
+    return false;
+  }
+  if (!reqBody.hasOwnProperty("productName")) {
+    // Does not have the property "productName"
+    return false;
+  }
+  // Passed all validation
+  return true;
+}
+module.exports = validateFormInput;
