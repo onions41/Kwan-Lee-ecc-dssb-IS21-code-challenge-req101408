c1d23e91c8f03a90cd8498567661154c06b1c6da
onions41 <kblee89@outlook.com>
2023-06-26 09:42:53 -0700

    Proper error message handling

--- react_frontend/src/productsTable/editProductModal/EditProdErrorDialog.jsx
+++ react_frontend/src/productsTable/editProductModal/EditProdErrorDialog.jsx
@@ -5,26 +5,25 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
-// Pop-up dialog. Confirms before clearing form fields
-export default function EditProdErrorDialog({ isOpen, setIsOpen, errorType, errorMsg, setIsModalOpen }) {
+// Pop-up dialog. Indicates failure to edit the product
+export default function EditProdErrorDialog({ isOpen, setIsOpen, errorRef }) {
   return (
     <Dialog
       open={isOpen}
-      onClose={() => setIsOpen(false)}
       aria-labelledby="edit-product-error-dialog"
       aria-describedby="edit-product-error-dialog-description"
     >
-      <DialogTitle id="edit-product-error-dialog">{errorType}</DialogTitle>
+      <DialogTitle id="edit-product-error-dialog">Error. Product was not edited.</DialogTitle>
       <DialogContent>
         <DialogContentText id="edit-product-error-dialog-description">
-          {"The product was not edited. Error message: " + errorMsg}
+          {errorRef.current}
         </DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button
           onClick={() => {
+            errorRef.current = "";
             setIsOpen(false);
-            setIsModalOpen(false);
           }}
         >
           Okay
