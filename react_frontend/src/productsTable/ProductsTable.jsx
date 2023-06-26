08cd56d5e44f4972e12aea8198da70ffcbe56a1a
onions41 <kblee89@outlook.com>
2023-06-26 10:15:16 -0700

    Additional comments

--- react_frontend/src/productsTable/ProductsTable.jsx
+++ react_frontend/src/productsTable/ProductsTable.jsx
@@ -10,6 +10,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+
+// Table library
 import { TableVirtuoso } from "react-virtuoso";
 
 // Internal imports
